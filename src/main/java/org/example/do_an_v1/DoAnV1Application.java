e6af9ba952233207cde392ddc90365dad143920b
Nam <lainguyennam270203@gmail.com>
2025-09-21 10:51:15 +0700

    first commit

---
+++ src/main/java/org/example/do_an_v1/DoAnV1Application.java
@@ -0,0 +1,13 @@
+package org.example.do_an_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoAnV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/do_an_v1/DoAnV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.do_an_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
