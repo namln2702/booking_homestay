28f7e80f1dce684dc8f75f180ddb52dda2030505
Nam <lainguyennam270203@gmail.com>
2025-11-03 22:13:58 +0700

    add payment controller

--- src/main/java/org/example/do_an_v1/controller/PaymentController.java
+++ src/main/java/org/example/do_an_v1/controller/PaymentController.java
@@ -1,11 +1,18 @@
 package org.example.do_an_v1.controller;
 
 
+import org.example.do_an_v1.payload.ApiResponse;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/payment")
 @RestController
 public class PaymentController {
 
+    @PostMapping("/customer_to_admin")
+    ApiResponse<?> customerPaymentAdmin(@RequestParam(name = "idTransaction") Long idTracsaction){
+        return new ApiResponse<>(200, "Okee", null);
+    }
 }
